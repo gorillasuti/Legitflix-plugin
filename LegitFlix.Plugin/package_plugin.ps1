15e15dce1332422f4e350754117237a0dcf3214e
gorillasuti <kiralydaniel517@gmail.com>
2026-02-11 00:39:35 +0100

    v1.0.0.24

--- LegitFlix.Plugin/package_plugin.ps1
+++ LegitFlix.Plugin/package_plugin.ps1
@@ -1,4 +1,4 @@
-$version = "1.0.0.23"
+$version = "1.0.0.24"
 $dllName = "LegitFlix.Plugin.dll"
 $zipName = "LegitFlix.Plugin_${version}.zip"
 # Adjust build dir to be relative to the script location if running from root
